06970141f3caf6f3728358aaa91907cbb1e3dca3
CH VAMSI <chokkapuvamsi123@gmail.com>
2023-12-01 14:53:31 +0530

    spring cloud gateway

---
+++ api-gateway/src/main/java/com/MicroServices/apigateway/apiGatewayConfiguration.java
@@ -0,0 +1,21 @@
+package com.MicroServices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class apiGatewayConfiguration {
+    @Bean
+    public RouteLocator getwayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(
+                        p->p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .build();
+
+    }
+}
